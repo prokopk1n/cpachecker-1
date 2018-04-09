a5c4d0b8fa5da41562ec24e84b09844405fde264
Peter Melnichenko <mpeterval@gmail.com>
2018-04-09 11:53:10 +0300

    Add a script for generating aspect files from null-deref annotations

---
+++ scripts/null-deref/aspects.py
@@ -0,0 +1,59 @@
+import argparse
+import json
+
+def load_annotations(path):
+    print("Loading annotations from {}".format(path))
+
+    with open(path) as f:
+        return json.load(f)
+
+def write_aspects(annotations, path, assert_func):
+    print("Writing aspect file {} using function {}".format(path, assert_func))
+
+    with open(path, "w") as f:
+        f.write('before: file ("$this")\n')
+        f.write('{\n')
+        f.write('#include "ldv.h"\n')
+        f.write('}\n')
+        f.write('\n')
+
+        for name, source_files in sorted(annotations.items()):
+            source_file, function = min(source_files.items())
+            need_aspect = False
+
+            for index, parameter in enumerate(function["params"]):
+                if parameter["is pointer"] and parameter["must deref"]:
+                    if not need_aspect:
+                        need_aspect = True
+
+                        f.write('around: call({}(..))\n'.format(name))
+                        f.write('{\n')
+
+                    f.write('  {}($arg{});\n'.format(assert_func, index + 1));
+
+            if need_aspect:
+                f.write('}\n')
+                f.write('\n')
+
+def main():
+    parser = argparse.ArgumentParser(description="Aspect file generator")
+
+    parser.add_argument(
+        "annotations",
+        help="Path to a JSON file with collected annotations.")
+
+    parser.add_argument(
+        "assert_aspect",
+        help="Path used to write generated aspects using ldv_assert.")
+
+    parser.add_argument(
+        "assume_aspect",
+        help="Path used to write generated aspects using ldv_assume.")
+
+    args = parser.parse_args()
+    annotations = load_annotations(args.annotations)
+    write_aspects(annotations, args.assert_aspect, "ldv_assert")
+    write_aspects(annotations, args.assume_aspect, "ldv_assume")
+
+if __name__ == "__main__":
+    main()
