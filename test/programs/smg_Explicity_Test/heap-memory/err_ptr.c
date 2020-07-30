e5e6fd589cee30d5a093b95cac71d83e2df5ec0d
Anton Vasilyev <vasilyev@ispras.ru>
2020-07-30 19:48:10 +0300

    SMGCPA: test for err ptr

---
+++ test/programs/smg_Explicity_Test/heap-memory/err_ptr.c
@@ -0,0 +1,68 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+long ldv_is_err(void const *ptr)
+{
+
+  return (long)((unsigned long)ptr > 4294967295UL);
+}
+
+
+void *ldv_err_ptr(long error)
+{
+
+  __VERIFIER_assume(error < 0L);
+  __VERIFIER_assume(error > -4095L);
+
+  return (void *)(4294967295L - error);
+}
+
+
+long ldv_ptr_err(void const *ptr)
+{
+
+  __VERIFIER_assume((unsigned long)ptr > 4294967295UL);
+
+  return (long)(4294967295UL - (unsigned long)ptr);
+}
+
+typedef unsigned char __uint8_t;
+typedef __uint8_t uint8_t;
+struct const_passdb
+{
+  char const
+  *filename;
+  char def[9U];
+  uint8_t off[9U];
+  uint8_t numfields;
+  uint8_t size_of;
+};
+static struct const_passdb const const_pw_db = { . filename = ( char const *) "/etc/passwd" ,
+    . def = "SsIIsss" ,
+    . off = { [ 0U ] = 0U , [ 1U ] = 8U , [ 2U ] = 16U ,
+              [ 3U ] = 20U , [ 4U ] = 24U , [ 5U ] = 32U , [ 6U ] = 40U } ,
+    . numfields = 7U , . size_of = 48U };
+
+int main() {
+    char *test;
+    int temp = __VERIFIER_nondet_int();
+    if (temp < 0)
+    test = malloc(10);
+    if (test) {
+        if ( const_pw_db.off[0] == 0  &&
+             const_pw_db.off[1] == 8  &&
+             const_pw_db.off[2] == 16 &&
+             const_pw_db.off[3] == 20 &&
+             const_pw_db.off[4] == 24 &&
+             const_pw_db.off[5] == 32 &&
+             const_pw_db.off[6] == 40 ) {
+            free(test);
+        }
+    }
+    return 0;
+}
